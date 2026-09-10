886a552e74151effebc7e252f9ef7c466332676f
VivianSVZ <vonzelewski.vivian@gmail.com>
2026-09-10 21:18:32 +0200

    Fix import path for App component in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import "./storage-polyfill.js";
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "App.jsx";
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
