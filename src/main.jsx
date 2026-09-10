71ca7df37d28364b202cf8064ccfbca11752217d
VivianSVZ <vonzelewski.vivian@gmail.com>
2026-09-10 20:48:17 +0200

    Update import paths for storage-polyfill and App

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 // Polyfill zuerst importieren, damit window.storage bereitsteht, bevor App
 // beim Mounten die erste Abfrage macht.
-import "./storage-polyfill.js";
+import "storage-polyfill.js";
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
